ba3f2e673749a25388dc1884dd2545b52a05dabd
Passamon Sukmaksin <passamon.smk@gmail.com>
2021-10-05 14:23:33 +0700

    initial commit

---
+++ weeklyassignment/magicsquarev1.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include<math.h>
+//จงเขียนโปรแกรมเพื่อตรวจสอบว่าเป็น จตุรัสกล หรือไม่ (Level 5)
+int main()
+{
+    int m, n;
+    scanf("%d", &n);
+    m = (n*((n*n)+1))/2;
+    int s[70][70], i, j, sum, count=0;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d", &s[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            sum += s[i][j];
+        }
+        if(sum==m)
+        {
+            count++;
+            sum=0;
+        }
+    }
+    for(j=0;j<n;j++)
+    {
+        for(i=0;i<n;i++)
+        {
+            sum += s[i][j];
+        }
+        if(sum==m)
+        {
+            count++;
+            sum=0;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i==j)
+            {
+                sum += s[i][j];
+            }
+        }
+        if(sum==m)
+        {
+            count++;
+            sum=0;
+        }
+    }
+        for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(i+j==n-1)
+            {
+                sum += s[i][j];
+            }
+        }
+        if(sum==m)
+        {
+            count++;
+            sum=0;
+        }
+    }
+    if(count==(n*2)+2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
